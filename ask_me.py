ab53e0d4a5c9c7f31d7a99a2e918fb0c412cb824
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-13 17:05:57 +0300

    Update ask_me.py

--- ask_me.py
+++ ask_me.py
@@ -1,2 +1,5 @@
+# the name is a variable name and that means 
+# whatever name entered will be stored on the local drive storage
 name = input("Please enter your name: ")
+# this prints hello and the name entered
 print("Hello,", name)
