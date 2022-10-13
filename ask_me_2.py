360157172a3e605ec4cebf52ab330397a043f4df
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-13 17:09:05 +0300

    Update ask_me_2.py

--- ask_me_2.py
+++ ask_me_2.py
@@ -3,9 +3,11 @@
 #     print("Hello, " , name)
 # except ValueError:
 #     print("Hello, stranger.")
-
+# we prompt a user to enter their name 
 name = input("Enter the name: ")
-
+# incase the user doesn't enter a name 
+# the program prints hello stranger 
+# incase name is entered it prints hello and the users name 
 if name == "":
     print ("Hello Stranger")
 else:
