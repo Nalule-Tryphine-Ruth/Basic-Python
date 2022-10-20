85b1e12de4c780b9ede8af72580b9dbe5c6b210e
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-10-20 18:55:20 +0300

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,9 @@
+# state the start and end values
+start, end = 1, 10
+ 
+# iterating each number in list
+for num in range(start, end + 1):
+     
+    # checking condition
+    if num % 2 != 0:
+        print(num, end = " ")
