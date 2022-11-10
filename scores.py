1320529f3a2d753c8c2272751b9e2533606d4b31
Nalule-Tryphine-Ruth <113919554+Nalule-Tryphine-Ruth@users.noreply.github.com>
2022-11-10 21:29:57 +0300

    Update scores.py

--- scores.py
+++ scores.py
@@ -3,21 +3,21 @@ inputLine = ""
 while inputLine != "stop":
 #     this is a grading program 
 # it helps users to grade according to the scores
-    score = float(input("Please enter your score: "))
-    if score >= 0 and score <= 1.0:
-        if score >= 0.9:
+    score = int(input("Please enter your score: "))
+    if score >= 0 and score <= 100:
+        if score >= 90:
             print("A")
-        elif score >= 0.8:
+        elif score >= 70:
             print("B")
-        elif score >= 0.7:
+        elif score >= 50:
             print("C")
-        elif score >= 0.6:
+        elif score >= 30:
             print("D")
         else:
             print("F")
         inputLine = input("Enter stop to discontinue and any other value to continue.")
     else:
-        print("please enter a number between 0 and 1.0")
+        print("please enter a number between 0 and 100")
     
 
 
